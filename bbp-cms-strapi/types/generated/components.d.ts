06d00d199918c6372be3b7d2ec990235ad5344de
whenzer <kled0ng123@gmail.com>
2026-04-13 13:59:56 +0800

    feat: add article and tag content types with controllers, services, and routes

--- bbp-cms-strapi/types/generated/components.d.ts
+++ bbp-cms-strapi/types/generated/components.d.ts
@@ -1,3 +1,37 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface TypeImage extends Struct.ComponentSchema {
+  collectionName: 'components_type_images';
+  info: {
+    displayName: 'Image';
+  };
+  attributes: {
+    Image: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    Name: Schema.Attribute.String;
+    Title: Schema.Attribute.String;
+  };
+}
+
+export interface TypeText extends Struct.ComponentSchema {
+  collectionName: 'components_type_texts';
+  info: {
+    displayName: 'Text';
+  };
+  attributes: {
+    Name: Schema.Attribute.String & Schema.Attribute.Required;
+    Text: Schema.Attribute.Blocks;
+    Title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'type.image': TypeImage;
+      'type.text': TypeText;
+    }
+  }
+}
